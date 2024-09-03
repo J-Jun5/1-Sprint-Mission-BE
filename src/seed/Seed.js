e8adf1018c19508575416a6f1062c31b137e046d
Jun5 <chdh1005@gmail.com>
2024-09-03 18:12:07 +0900

    :sparkles: [Feat] 시딩 스크립트 작성 및 Product 스키마 추가

---
+++ src/seed/Seed.js
@@ -0,0 +1,59 @@
+import { PrismaClient } from "@prisma/client";
+import { resetIdSequence } from "utils/ResetIdSequence";
+
+import { articleDataList } from "seed/ArticleData";
+import { commentDataList } from "seed/CommentData";
+import { productDataList } from "seed/ProductData";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // 기존 데이터 삭제
+  try {
+    await prisma.product.deleteMany({});
+    console.log("✅ product 데이터 삭제 완료되었습니다.");
+    await prisma.article.deleteMany();
+    console.log("✅ article 데이터 삭제 완료되었습니다.");
+    await prisma.comment.deleteMany();
+    console.log("✅ comment 데이터 삭제 완료되었습니다.");
+  } catch (error) {
+    console.error("⛔ 기존 데이터 삭제 실패:", error);
+  }
+
+  //ID 시퀀스 초기화
+  await resetIdSequence();
+
+  // 초기 데이터 삽입
+  try {
+    await prisma.product.createMany({
+      data: productDataList(10),
+      skipDuplicates: true,
+    });
+    console.log("✅✅ product 데이터 시딩 완료");
+
+    await prisma.article.createMany({
+      data: articleDataList(10),
+      skipDuplicates: true,
+    });
+    console.log("✅✅ article 데이터 시딩 완료");
+
+    await prisma.comment.createMany({
+      data: commentDataList(10),
+      skipDuplicates: true,
+    });
+    console.log("✅✅ comment 데이터 시딩 완료");
+  } catch (error) {
+    console.error("⛔⛔ 초기 데이터 시딩 오류:", error);
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+    console.log("✅✅✅ 시딩 ALL COMPLETED. ");
+  })
+  .catch(async (error) => {
+    console.error("⛔⛔⛔ 시딩 오류 : ", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
